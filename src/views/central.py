3f9014ca8591a365635861fa178d34c2bcc1a02e
Lavínia Beghini <lavinia.beghini@gmail.com>
2020-12-24 19:31:52 -0300

    Implements main window

---
+++ src/views/central.py
@@ -0,0 +1,43 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5 import QtCore
+
+
+class Central(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        title = QLabel("<h1>Graph Filter</h1>")
+        title.setAlignment(QtCore.Qt.AlignCenter)
+
+        version = QLabel("<h2>version 1.0</h2>")
+        version.setAlignment(QtCore.Qt.AlignCenter)
+
+        logo = QLabel("")
+        logo.setPixmap(QPixmap("views/resources/icons/hexagono.png").scaled(64, 64, QtCore.Qt.KeepAspectRatio))
+        logo.setAlignment(QtCore.Qt.AlignCenter)
+
+        open_button = QPushButton("Open")
+        open_button.setIcon(QIcon("views/resources/icons/folder.png"))
+        open_button.setMinimumHeight(50)
+        open_button.setMinimumWidth(100)
+
+        new_button = QPushButton("New Project")
+        new_button.setIcon(QIcon("views/resources/icons/mais.png"))
+        new_button.setMinimumHeight(50)
+        new_button.setMinimumWidth(150)
+
+        layout = QVBoxLayout(self)
+        layout.addStretch(3)
+        layout.addWidget(logo)
+        layout.addStretch(2)
+        layout.addWidget(title)
+        layout.addWidget(version)
+        layout.addStretch(3)
+        layout.addWidget(open_button)
+        layout.addStretch(1)
+        layout.addWidget(new_button)
+        layout.addStretch(3)
+        layout.setAlignment(open_button, QtCore.Qt.AlignCenter)
+        layout.setAlignment(new_button, QtCore.Qt.AlignCenter)
--- src/views/main_window.py
+++ src/views/main_window.py
@@ -1,66 +1,29 @@
-from src.views.header import Header
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap, QIcon
-from PyQt5 import QtCore
+from src.views import central
 
 
-def test():
-    print("teste")
-
-
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+
         self.title_bar = "Graph Filter"
+        self.icon = QtGui.QIcon("views/resources/icons/hexagono.png")
 
         self.width = 600
         self.height = 600
 
+        rectangle = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        rectangle.moveCenter(center_point)
+        self.move(rectangle.topLeft())
+
         self.setObjectName("main_window")
-
-        self.title = QLabel("<h1>Graph Filter</h1>")
-        self.title.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.version = QLabel("<h2>version 1.0</h2>")
-        self.version.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.logo = QLabel("")
-        self.logo.setPixmap(QPixmap("views/resources/icons/hexagono.png").scaled(64, 64, QtCore.Qt.KeepAspectRatio))
-        self.logo.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.open = QPushButton("Open")
-        self.open.setIcon(QIcon(QPixmap("views/resources/icons/folder.png")))
-        self.open.setFixedSize(200, 50)
-        self.open.clicked.connect(test)
-
-        self.new = QPushButton("New Project")
-        self.new.setIcon(QIcon(QPixmap("views/resources/icons/mais.png")))
-        self.new.setFixedSize(200, 50)
-        self.new.clicked.connect(test)
-
-        # stylesheet = ""
-        #
-        # with open("views/resources/stylesheet/design.qss", "r") as f:
-        #     stylesheet = f.read()
-        # self.setStyleSheet(stylesheet)
-
-        self.ui_components()
-
+        self.setCentralWidget(central.Central())
         self.init_window()
 
     def init_window(self):
-        self.setWindowIcon(QtGui.QIcon("views/resources/icons/hexagono.png"))
+        self.setWindowIcon(self.icon)
         self.setWindowTitle(self.title_bar)
-        self.setFixedSize(self.width, self.height)
-
-    def ui_components(self):
-        layout = QVBoxLayout(self)
-        layout.setAlignment(QtCore.Qt.AlignCenter)
-        layout.setSpacing(30)
-        layout.addWidget(self.logo)
-        layout.addWidget(self.title)
-        layout.addWidget(self.version)
-        layout.addWidget(self.open)
-        layout.addWidget(self.new)
+        self.setMinimumSize(self.width, self.height)
