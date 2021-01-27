7d8550e80b498114bc0b90530c553451cb749f3e
Átila Arueira Jones <atila.jones@ifsudestemg.edu.br>
2021-01-27 18:46:04 -0300

    Combo box can load a g6 file

--- src/views/windows/project/visualize/visualize.py
+++ src/views/windows/project/visualize/visualize.py
@@ -25,8 +25,22 @@ class Visualize(QWidget):
             self.tool_bar.layout().setSpacing(30)
             self.tool_bar.layout().setContentsMargins(15, 10, 20, 20)
             self.tool_bar.setMovable(False)
-            self.tool_bar.addWidget(QLabel("Graphs"))
-            self.tool_bar.addWidget(QComboBox())
+            self.tool_bar.addWidget(QLabel("View graphs from .g6 file"))
+
+            self.g6_file_load_button = QPushButton("Load")
+            self.g6_file_load_button.clicked.connect(self.load_file)
+            
+            self.graphs_list = QComboBox()
+            self.graphs_list.adjustSize()
+            self.graphs_list.setMaximumWidth(500)
+
+
+            self.text_file_loaded = QLabel()
+            self.text_file_loaded.setText('(no file loaded)')
+
+            self.tool_bar.addWidget(self.g6_file_load_button)
+            self.tool_bar.addWidget(self.graphs_list)
+            self.tool_bar.addWidget(self.text_file_loaded)
             self.tool_bar.addSeparator()
             self.tool_bar.addAction(self.project_window.zoom_in_action)
             self.tool_bar.addAction(self.project_window.zoom_out_action)
@@ -48,3 +62,14 @@ class Visualize(QWidget):
 
             self.graph = None
             self.info = None
+
+    def load_file(self):
+        file_dialog = QFileDialog()
+        file_dialog.setNameFilters(["Text files (*.txt)", "Graph Filter (*.g6)"])
+        file_path = file_dialog.getOpenFileName(filter="Graph Filter (*.g6)")
+        if file_path[0] != '':
+            file = open(file_path[0], 'r')
+            self.text_file_loaded.setText('(File .../'+file.name.split('/')[-1]+')')
+            self.graphs_list.clear()
+            self.graphs_list.addItems(file.read().splitlines())
+
