4ac6d9582d8eb635e004808eb3e1f00e13467459
Átila Arueira Jones <atila.jones@ifsudestemg.edu.br>
2021-02-05 22:39:54 -0300

    Rest unicode
    TODO: encode UNICODE character UTF-8 from excel to python.

--- src/views/windows/project/dictionary/dictionary.py
+++ src/views/windows/project/dictionary/dictionary.py
@@ -1,9 +1,7 @@
-import sys
-
-from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui
-import xlrd
 import os
+import xlrd
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import *
 
 
 class Dictionary(QWidget):
@@ -20,32 +18,34 @@ class Dictionary(QWidget):
         self.concepts_list = QListWidget()
         self.sheet = xlrd.open_workbook(os.getcwd() + '/views/resources/data_dictionary.xls').sheet_by_name('dic')
         self.title = QLabel()
-        self.title.setFont(QtGui.QFont("Arial", 16,))
-        self.title.setWordWrap(True);
+        self.title.setFont(QtGui.QFont("Arial", 20))
+        self.title.setWordWrap(True)
         self.definitions = QLabel()
-        self.definitions.setWordWrap(True);
-        self.definitions.setFont(QtGui.QFont("Arial", 12))
+        self.definitions.setWordWrap(True)
+        self.definitions.setFont(QtGui.QFont("Arial", 14))
         self.usages = QLabel()
-        self.usages.setWordWrap(True);
-        self.usages.setFont(QtGui.QFont("Arial", 12))
+        self.usages.setWordWrap(True)
+        self.usages.setFont(QtGui.QFont("Arial", 14))
         self.references = QLabel()
-        self.references.setWordWrap(True);
-        self.references.setFont(QtGui.QFont("Arial", 12))
-
+        self.references.setWordWrap(True)
+        self.references.setFont(QtGui.QFont("Arial", 14))
 
         self.create_concepts()
 
-        # TODO: learn how to control better the spacing in vbox items
-
         aside_layout = QVBoxLayout()
         aside_layout.addWidget(self.title)
+        aside_layout.addSpacing(30)
         aside_layout.addWidget(QLabel("<h2>Definitions</h2>"))
+        aside_layout.addSpacing(10)
         aside_layout.addWidget(self.definitions)
+        aside_layout.addSpacing(30)
         aside_layout.addWidget(QLabel("<h2>Usages</h2>"))
+        aside_layout.addSpacing(10)
         aside_layout.addWidget(self.usages)
+        aside_layout.addSpacing(30)
         aside_layout.addWidget(QLabel("<h2>References</h2>"))
         aside_layout.addWidget(self.references)
-        aside_layout.setSpacing(0)
+        aside_layout.addStretch(0)
 
         main_layout = QHBoxLayout()
         main_layout.addWidget(self.concepts)
@@ -54,32 +54,25 @@ class Dictionary(QWidget):
         self.setLayout(main_layout)
 
     def create_concepts(self):
-
         concepts_layout = QVBoxLayout()
 
         # NOTE: this code generate multiples items
         for i in range(1, self.sheet.nrows):
             self.concepts_list.insertItem(i, self.sheet.cell_value(i, 0))
 
-
+        # TODO connect with select from keyboard
         self.concepts_list.clicked.connect(self.on_clicked_concept)
 
         concepts_layout.addWidget(self.concepts_list)
         self.concepts.setLayout(concepts_layout)
         self.concepts_list.setCurrentItem(self.concepts_list.item(0))
-        self.title.setText(f"<h1>{self.concepts_list.currentItem().text()}<\h1>")
+        self.on_clicked_concept()
+        # self.title.setText(f"<h1>{self.concepts_list.currentItem().text()}<\h1>")
 
     def on_clicked_concept(self):
-        line_sheet = self.concepts_list.currentRow()+1
-        self.title.setText(f"<b>{self.concepts_list.currentItem().text()}<\b>")
-        self.definitions.setText(u"{0}".format(self.sheet.cell_value(line_sheet, 1)).encode("utf-8"))
+        line_sheet = self.concepts_list.currentRow() + 1
+        self.title.setText(self.concepts_list.currentItem().text())
+        self.definitions.setText(self.sheet.cell_value(line_sheet, 1))
         self.usages.setText(self.sheet.cell_value(line_sheet, 2))
         self.references.setOpenExternalLinks(True)
         self.references.setText('<a href={0}>{0}</a>'.format(self.sheet.cell_value(line_sheet, 3)))
-
-
-
-
-
-
-
