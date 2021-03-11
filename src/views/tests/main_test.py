dd62dac9ae09708107628fe4d75184bc9f388d5f
LBeghini <lavinia.beghini@gmail.com>
2021-03-10 22:50:03 -0300

    Create main_test.py

---
+++ src/views/tests/main_test.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+import os
+
+# NOTE:
+#  This code opens project from Wizard
+# from src.views.windows.main.index import Index
+# app = QApplication(sys.argv)
+# window = Index()
+
+# NOTE:
+#  This code opens project from Visualize
+from src.views.windows.project.project_window import ProjectWindow
+app = QApplication(sys.argv)
+window = ProjectWindow()
+file_dir = os.path.dirname(os.path.realpath('__file__'))
+current_file_dir = os.path.join(file_dir, '../../backend/tests/resources/graphs/graphs1.g6').replace('\\', '/')
+window.visualize.fill_combo([current_file_dir])
+
+# NOTE:
+#  Do not comment the following code
+window.show()
+# start the event loop
+app.exec_()
