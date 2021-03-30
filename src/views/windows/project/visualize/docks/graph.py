7d478b2476067c340873f4839eca050cfb758b03
LBeghini <lavinia.beghini@gmail.com>
2021-03-29 22:38:27 -0300

    Implement pyvis with modifications

--- src/views/windows/project/visualize/docks/graph.py
+++ src/views/windows/project/visualize/docks/graph.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import *
 import matplotlib
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 import networkx as nx
-import matplotlib.pyplot as plt
+from PyQt5.QtCore import QUrl
+from pyvis.network import Network
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 matplotlib.use('Qt5Agg')
 
@@ -15,15 +16,73 @@ class Graph(QDockWidget):
         self.visualize = visualize
         self.setWindowTitle("Graph")
 
-        self.figure = plt.figure()
-
-        self.canvas = FigureCanvasQTAgg(self.figure)
-
+        self.webView = QWebEngineView()
         self.setFeatures(QDockWidget.DockWidgetFloatable | QDockWidget.DockWidgetMovable)
-        self.setWidget(self.canvas)
+        self.setWidget(self.webView)
 
     def plot_graph(self, graph):
-        self.figure.clf()
+        net = Network()
+        net.width = "100%"
+        net.height = "100%"
+        net.set_options("var options = {}")
         g = nx.from_graph6_bytes(graph.encode('utf-8'))
-        nx.draw(g, with_labels=True)
-        self.canvas.draw_idle()
+        net.from_nx(g)
+        net.write_html('graph.html')
+
+        filepath = 'D:\Documentos\GitHub\GraphFilter.py\src\graph.html'.replace('\\', '/')
+
+        with open(filepath, "r") as f:
+            lines = f.read().splitlines()
+            lines[58] = """
+            var options = {
+                "configure": {
+                    "enabled": false
+                },
+                "edges": {
+                    "color": {
+                        "inherit": true
+                    },
+                    "smooth": {
+                        "enabled": false,
+                        "type": "continuous"
+                    }
+                },
+                "interaction": {
+                    "dragNodes": true,
+                    "hideEdgesOnDrag": false,
+                    "hideNodesOnDrag": false
+                },
+                "physics": {
+                    "enabled": true,
+                    "stabilization": {
+                        "enabled": true,
+                        "fit": true,
+                        "iterations": 1000,
+                        "onlyDynamicEdges": false,
+                        "updateInterval": 50
+                    }
+                },
+                manipulation: {
+                    enabled: true,
+                    initiallyActive: false,
+                    addNode: function(nodeData,callback) {
+                      nodeData.label = nodes.length;
+                      nodeData.size = 10;
+                      nodeData.shape = 'dot';
+                      callback(nodeData);
+                    },
+                    addEdge: true,
+                    editNode: undefined,
+                    editEdge: true,
+                    deleteNode: true,
+                    deleteEdge: true
+                }
+            };
+            """
+            data = "\n".join(lines)
+
+        with open(filepath, "w") as f:
+            f.write(data)
+
+        self.webView.load(QUrl(filepath))
+        self.webView.show()
