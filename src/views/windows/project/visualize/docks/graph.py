91f34d89c2276336290cab036ec02d5a485fc28c
LBeghini <lavinia.beghini@gmail.com>
2021-03-30 01:15:59 -0300

    Create own class to plot graphs

--- src/views/windows/project/visualize/docks/graph.py
+++ src/views/windows/project/visualize/docks/graph.py
@@ -2,8 +2,9 @@ from PyQt5.QtWidgets import *
 import matplotlib
 import networkx as nx
 from PyQt5.QtCore import QUrl
-from pyvis.network import Network
+from src.plot.network import Network
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+import os
 
 matplotlib.use('Qt5Agg')
 
@@ -22,67 +23,10 @@ class Graph(QDockWidget):
 
     def plot_graph(self, graph):
         net = Network()
-        net.width = "100%"
-        net.height = "100%"
-        net.set_options("var options = {}")
         g = nx.from_graph6_bytes(graph.encode('utf-8'))
-        net.from_nx(g)
-        net.write_html('graph.html')
+        net.create_vis_data(g)
 
-        filepath = 'D:\Documentos\GitHub\GraphFilter.py\src\graph.html'.replace('\\', '/')
+        filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../plot/plot.html"))
 
-        with open(filepath, "r") as f:
-            lines = f.read().splitlines()
-            lines[58] = """
-            var options = {
-                "configure": {
-                    "enabled": false
-                },
-                "edges": {
-                    "color": {
-                        "inherit": true
-                    },
-                    "smooth": {
-                        "enabled": false,
-                        "type": "continuous"
-                    }
-                },
-                "interaction": {
-                    "dragNodes": true,
-                    "hideEdgesOnDrag": false,
-                    "hideNodesOnDrag": false
-                },
-                "physics": {
-                    "enabled": true,
-                    "stabilization": {
-                        "enabled": true,
-                        "fit": true,
-                        "iterations": 1000,
-                        "onlyDynamicEdges": false,
-                        "updateInterval": 50
-                    }
-                },
-                manipulation: {
-                    enabled: true,
-                    initiallyActive: false,
-                    addNode: function(nodeData,callback) {
-                      nodeData.label = nodes.length;
-                      nodeData.size = 10;
-                      nodeData.shape = 'dot';
-                      callback(nodeData);
-                    },
-                    addEdge: true,
-                    editNode: undefined,
-                    editEdge: true,
-                    deleteNode: true,
-                    deleteEdge: true
-                }
-            };
-            """
-            data = "\n".join(lines)
-
-        with open(filepath, "w") as f:
-            f.write(data)
-
-        self.webView.load(QUrl(filepath))
+        self.webView.load(QUrl.fromLocalFile(filepath))
         self.webView.show()
