396739d607cd576a3ac6ccf06de150e47d66301f
Átila Arueira Jones <atila.jones@ifsudestemg.edu.br>
2021-04-06 16:30:13 -0300

    About text done

---
+++ src/views/windows/project/about/about.py
@@ -0,0 +1,5 @@
+title = "<h1>Graph Filter<\h1>"
+version = "<h2>Version 2.0.0<\h2>"
+text = "The main goal of this software is to give assistance to Graph Theory and Spectral Graph Theory researchers to establish or refute conjectures quickly and simply, providing for visualization a filtered list of graphs according to the properties given by the user."
+page = '<h3> Page: <\h3> \n <a href={0}>{0}</a>'.format('sistemas.jf.ifsudestemg.edu.br/graphfilter')
+github = '<h3> GitHub: <\h3>  \n <a href={0}>{0}</a>'.format('github.com/GraphFilter/GraphFilter.py')
