d936de332fd2d93b460dbefe9a9324bcb6305fce
LBeghini <lavinia.beghini@gmail.com>
2022-04-24 18:20:59 -0300

    Change PyQtGraph to Netgraph

--- source/view/project/docks/visualize_graph_dock.py
+++ source/view/project/docks/visualize_graph_dock.py
@@ -1,21 +1,19 @@
 from PyQt5.QtWidgets import *
-import matplotlib
-import networkx as nx
-import numpy as np
-import pyqtgraph as pg
-from source.domain.graph import Graph
+import matplotlib; matplotlib.use("Qt5Agg")
 
-matplotlib.use('Qt5Agg')
+import networkx as nx
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+from netgraph import InteractiveGraph
+from PyQt5.QtCore import Qt
 
 
 class VisualizeGraphDock(QDockWidget):
 
     def __init__(self):
         super().__init__()
-        self.graphic_layout_widget = pg.GraphicsLayoutWidget(show=True)
-        self.view_box = self.graphic_layout_widget.addViewBox()
 
-        self.graph = Graph()
+        self.canvas = MplCanvas(self, None, width=5, height=4, dpi=100)
 
         self.set_content_attributes()
 
@@ -23,43 +21,23 @@ class VisualizeGraphDock(QDockWidget):
         self.setWindowTitle("Visualize")
         self.setObjectName("Visualize")
 
-        self.graphic_layout_widget.setBackground('w')
-
-        self.view_box.setAspectLocked()
-
         self.setFeatures(QDockWidget.DockWidgetFloatable | QDockWidget.DockWidgetMovable | QDockWidget.DockWidgetClosable)
 
-        self.setWidget(self.graphic_layout_widget)
+        aux = QVBoxLayout()
+        aux.addWidget(self.canvas, alignment=Qt.AlignCenter)
+
+        self.setLayout(aux)
 
     def plot_graph(self, graph):
+        self.canvas = MplCanvas(self, nx.from_graph6_bytes(graph.encode('utf-8')))
+
+
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, graph=None, width=5, height=9, dpi=100):
+        super(MplCanvas, self).__init__(Figure(figsize=(width, height), dpi=dpi))
+        self.setParent(parent)
+        self.ax = self.figure.add_subplot(111)
+        self.ax.clear()
         if graph is None:
-            self.view_box.removeItem(self.graph)
             return
-
-        g = nx.from_graph6_bytes(graph.encode('utf-8'))
-        self.view_box.addItem(self.graph)
-        self.define_graph(g)
-
-    def define_graph(self, graph):
-        position = []
-        adjacency = []
-        indexes = []
-
-        points = nx.drawing.layout.spring_layout(graph)
-        for i, point in enumerate(points.values()):
-            aux = [point[0] * 10 // 1, point[1] * 10 // 1]
-            position.append(aux)
-            indexes.append(i)
-
-        edges = graph.edges(data=True)
-        for edge in edges:
-            aux = [edge[0], edge[1]]
-            if not reversed(aux) in adjacency:
-                adjacency.append(aux)
-
-        pos = np.array(position, dtype=float)
-        adj = np.array(adjacency)
-
-        texts = ["%d" % i for i in indexes]
-
-        self.graph.setData(pos=pos, adj=adj, size=1, pxMode=False, text=texts)
+        self.plot_instance = InteractiveGraph(graph, ax=self.ax)
