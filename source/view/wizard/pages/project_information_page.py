38899b820c7f4675a1b4c2158f57c79be019f2d3
Igor Rosa <igorrosafilgueiras@gmail.com>
2022-11-14 16:39:00 -0300

    Update project_information_page.py

--- source/view/wizard/pages/project_information_page.py
+++ source/view/wizard/pages/project_information_page.py
@@ -39,7 +39,7 @@ class ProjectInformationWizardPage(QWizardPage):
         form.addRow(QLabel("Project Name"), self.project_name_input)
         form.addRow(QLabel("Project location"), file_line)
         form.addRow(QLabel("Project description \n(optional)"), self.project_description_input)
-        form.setContentsMargins(12, 25, 12, 150)
+        form.setContentsMargins(12, 25, 12, 200)
 
         self.setLayout(form)
 
