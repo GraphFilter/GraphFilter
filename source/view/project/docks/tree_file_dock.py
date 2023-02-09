0e11fefd298ec294de476290faeaa9aab2a7aed4
Igor Rosa <igorrosafilgueiras@gmail.com>
2023-02-09 20:10:22 -0300

    Load file function on tree file dock

--- source/view/project/docks/tree_file_dock.py
+++ source/view/project/docks/tree_file_dock.py
@@ -1,4 +1,5 @@
-import os
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QCursor
 
 from PyQt5.QtWidgets import *
 
@@ -24,7 +25,33 @@ class TreeFileDock(QDockWidget):
         self.model.setRootPath(self.path)
 
         self.tree = QTreeView()
+        self.tree.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.tree.customContextMenuRequested.connect(self.context_menu)
+
         self.tree.setModel(self.model)
         self.tree.setRootIndex(self.model.index(self.path))
+        self.tree.doubleClicked.connect(self.handle_double_click)
+
+        self.tree.hideColumn(1)
+        self.tree.hideColumn(2)
+        self.tree.hideColumn(3)
 
         self.setWidget(self.tree)
+
+    def context_menu(self):
+        menu = QMenu()
+        load_file = menu.addAction("Load File on Graph Filter")
+        load_file.triggered.connect(self.load_file_on_combo)
+        cursor = QCursor()
+        menu.exec(cursor.pos())
+
+    def handle_double_click(self):
+        index = self.tree.currentIndex()
+        type_item = self.model.type(index)
+        if type_item == "g6 File" or type_item == "json File":
+            self.load_file_on_combo()
+        else:
+            pass
+
+    def load_file_on_combo(self):
+        print("open on combo box")
