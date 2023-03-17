aa0034264aa4674b526119a57cc1bffc666f9c71
Igor Rosa <igorrosafilgueiras@gmail.com>
2023-03-17 17:14:45 -0300

    Reading json on tree WIP

--- source/view/project/docks/tree_file_dock.py
+++ source/view/project/docks/tree_file_dock.py
@@ -1,5 +1,9 @@
+import json
+
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor
+from source.view.project.project_tool_bar import ProjectToolBar
+from source.controller.filter_controller import FilterController
 
 from PyQt5.QtWidgets import *
 
@@ -41,17 +45,42 @@ class TreeFileDock(QDockWidget):
     def context_menu(self):
         menu = QMenu()
         load_file = menu.addAction("Load File on Graph Filter")
-        load_file.triggered.connect(self.load_file_on_combo)
+        load_file.triggered.connect(self.handle_double_click)
         cursor = QCursor()
         menu.exec(cursor.pos())
 
     def handle_double_click(self):
         index = self.tree.currentIndex()
         type_item = self.model.type(index)
-        if type_item == "g6 File" or type_item == "json File":
-            self.load_file_on_combo()
+        if type_item == "json File":
+            self.load_file_on_combo(index, type_item)
+        if type_item == "g6 File":
+            self.load_file_on_combo(index, type_item)
         else:
             pass
 
-    def load_file_on_combo(self):
-        print("open on combo box")
+    def load_file_on_combo(self, index, type):
+        file_path = self.model.filePath(index)
+        #print(file_path)
+        #tuple(file_path)
+        if type == "json File":
+            f = open(file_path)
+            data = json.load(f)
+            graphs = tuple(data['filtered_graphs'])
+
+            ProjectToolBar.fill_combo_graphs(graphs)
+
+
+        #    with open(file_path) as file:
+        #        content = file.read()
+        #        data = json.loads(content)
+        #        project_information_store.fill_data(data)
+        #        ProjectToolBar.fill_combo_graphs(project_information_store.filtered_graphs)
+                #ProjectToolBar.fill_combo_graphs(project_information_store.graph_files)
+        #else:
+        #    project_information_store.graph_files += file_path.read().splitlines()
+
+        #FilterController.start_filter()
+        #ProjectToolBar.fill_combo_graphs(project_information_store.graph_files)
+
+
