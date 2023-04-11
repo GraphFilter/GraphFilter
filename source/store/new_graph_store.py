bd3cf21de4e0062ae09673242894a8524f64f81f
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-04-11 14:31:48 -0300

    Implementing triangular lattice graph

--- source/store/new_graph_store.py
+++ source/store/new_graph_store.py
@@ -128,6 +128,26 @@ class Grid2dGraph(NewGraphStore):
         dialog.close()
 
 
+class TriangularLatticeGraph(NewGraphStore):
+    name = "Triangular Lattice Graph"
+
+    @staticmethod
+    def open_dialog():
+        dialog = NewGraphDialog(name=TriangularLatticeGraph.name, m='', n='', periodic='False', with_positions='True')
+        dialog.dialog_next_button.clicked.connect(lambda: TriangularLatticeGraph.create_graph(dialog))
+        dialog.exec()
+
+    @staticmethod
+    def create_graph(dialog):
+        new_graph_store.set_graph(nx.triangular_lattice_graph(int(dialog.dict['m'].text()),
+                                                              int(dialog.dict['n'].text()),
+                                                              bool(dialog.dict['periodic'].text()),
+                                                              bool(dialog.dict['periodic'].text())))
+        new_graph_store.set_file_path(project_information_store.project_location + f"\\{dialog.dict['name'].text()}.g6")
+
+        dialog.close()
+
+
 new_graph_store = NewGraphStore()
 
 new_graph_dict_name = new_graph_store.dic_name_new_graph
