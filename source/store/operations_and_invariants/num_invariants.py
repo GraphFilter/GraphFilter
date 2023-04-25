1dfefc53165bb31232020c60bd94f7fbc17f3b2c
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-04-25 20:43:56 -0300

    Implementing SmallestEigenE

--- source/store/operations_and_invariants/num_invariants.py
+++ source/store/operations_and_invariants/num_invariants.py
@@ -779,6 +779,23 @@ class SmallestEigenDQ(InvariantNum):
         return Utils.print_numeric(SmallestEigenDQ.calculate(graph), precision)
 
 
+class SmallestEigenE(InvariantNum):
+    name = "Smallest E-eigenvalue"
+    code = "\u03b5\u2099"
+    type = "number_spectral"
+
+    @staticmethod
+    def calculate(graph):
+        if nx.is_connected(graph):
+            return Utils.smallest_eigen(inv_other.EccentricityMatrix.calculate(graph))
+        else:
+            return 10 ** 10
+
+    @staticmethod
+    def print(graph, precision):
+        return Utils.print_numeric(SmallestEigenE.calculate(graph), precision)
+
+
 class AdjacencyEnergy(InvariantNum):
     name = 'A-Energy'
     code = 'EA'
