6265aaf61ddde8ec1907af11850af8a2b498852b
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-06-09 14:06:09 -0300

    Create tooltip to file name

--- source/view/project/project_tool_bar.py
+++ source/view/project/project_tool_bar.py
@@ -153,8 +153,9 @@ class ProjectToolBar(QToolBar):
         return graphs[0]
 
     def set_file_label(self, file_name):
-        self.filtered_graphs_label.setMaximumWidth(100)
+        self.filtered_graphs_label.setMaximumWidth(75)
         self.filtered_graphs_label.setText(f"      File \n {file_name}")
+        self.filtered_graphs_label.setToolTip(f"{file_name}")
 
     @staticmethod
     def set_button_style(button):
