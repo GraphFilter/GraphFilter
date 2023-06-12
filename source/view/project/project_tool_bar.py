989bf37843fab5b3352665fb2f0aff50785ad037
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-06-12 15:20:29 -0300

    Set fixed size to name file label on toolbar

--- source/view/project/project_tool_bar.py
+++ source/view/project/project_tool_bar.py
@@ -153,7 +153,7 @@ class ProjectToolBar(QToolBar):
         return graphs[0]
 
     def set_file_label(self, file_name):
-        self.filtered_graphs_label.setMaximumWidth(75)
+        self.filtered_graphs_label.setFixedWidth(75)
         self.filtered_graphs_label.setText(f"      File \n {file_name}")
         self.filtered_graphs_label.setToolTip(f"{file_name}")
 
