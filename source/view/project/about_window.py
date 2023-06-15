91d64a89c8732673c0240769b00d730fad01bc92
Átila Arueira Jones <atila.jones@ifsudestemg.edu.br>
2023-06-14 21:30:12 -0300

    About window implemented

--- source/view/project/about_window.py
+++ source/view/project/about_window.py
@@ -7,28 +7,33 @@ class AboutWindow(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.data_title = r"<h1>Graph Filter<\h1>"
-        self.data_version = r"<h2>Version 3.0.0<\h2>"
-        self.data_text = "The goal of this software is to help researchers of Graph Theory and Graph Spectral " \
-                         "Theory establish or refute conjectures. The program allows, in an intuitive way, the" \
-                         " visualization and manipulation of graphs, using operations and a diversity of invariants. " \
-                         "It also allows performing graph filtering according to properties given by the user."
+        self.data_title = r"<h1>Graph Filter</h1>"
+        self.data_version = r"<h2>Version 3.0.0</h2>"
+        self.data_text = r" <p align='justify'> The goal of this software is to help researchers of Graph Theory and " \
+                         r"Graph Spectral Theory establish or refute conjectures. The program allows, in an intuitive " \
+                         r"way, the visualization and manipulation of graphs, using operations and a diversity of " \
+                         r"invariants. It also allows performing graph filtering according to properties given by " \
+                         r"the user. </p>"
         self.data_page = \
             r'<h3> Page <\h3> <br> <a href={0}>{0}</a>'.format('sistemas.jf.ifsudestemg.edu.br/graphfilter')
         self.data_github = \
             r'<h3> GitHub <\h3>  <br> <a href={0}>{0}</a>'.format('github.com/GraphFilter/GraphFilter.py')
 
-        self.data_authors = r"<h3>Authors<\h3> <br> " \
-                                 r"Átila A. Jones <i>(v1.0 forward)</i>: atila.jones@ifsudestemg.edu.br <br>" \
-                                 r"Lavínia Beghini de Castro <i>(v2.0 forward)</i> <br>" \
-                                 r"Fernando S. Pimenta <i>(v2.0 forward)</i> <br>" \
-                                 r"Igor Rosa F. Pinto <i>(v2.0 forward)</i> <br>" \
-                                 r"Denilson P. O. Ribeiro  <i>(v1.0)</i>"
+        self.data_authors = r"<b>Authors</b>" \
+                                 r"<ul> <li>Átila A. Jones </b><i>(v1.0 forward)</i> -" \
+                            r" atila.jones@ifsudestemg.edu.br </li>"\
+                                 r"<li>Lavínia Beghini de Castro </b><i>(v2.0 forward)</i> </li>" \
+                                 r"<li>Fernando S. Pimenta </b><i>(v2.0 forward)</i> </li>" \
+                                 r"<li>Igor Rosa F. Pinto </b><i>(v2.0 forward)</i> </li>" \
+                                 r"<li>Denilson P. O. Ribeiro </b><i>(v1.0)</i> </ul>"
 
-        self.data_text2 = "This program is the result of research projects carried out at the Instituto Federal do" \
-                          "Sudeste de Minas Gerais, Juiz de Fora - MG, Brazil. With financial support from the " \
-                          "institution, as well as FAPEMIG and CNPq."
+        self.data_text2 = r"<p align='justify'> This program is the result of research projects at Instituto Federal " \
+                          r"do Sudeste de Minas Gerais, Juiz de Fora - MG, Brazil. With financial support from the " \
+                          "institution, as well as FAPEMIG and CNPq.</p>"
 
+        self.logo_cnpq = QLabel()
+        self.logo_fapemig = QLabel()
+        self.logo_if = QLabel()
 
         self.set_content_attributes()
         self.set_up_layout()
@@ -50,21 +55,34 @@ class AboutWindow(QDialog):
         about_version = QLabel(self.data_version)
         about_text = QLabel(self.data_text)
         about_authors = QLabel(self.data_authors)
-        about_text2 = QLabel(self.data_text2)
         about_text.setWordWrap(True)
         about_page = QLabel(self.data_page)
         about_github = QLabel(self.data_github)
+        about_text2 = QLabel(self.data_text2)
+        about_text2.setWordWrap(True)
 
-        # self.logo.setPixmap(QPixmap("resources/logos/graph_filter.png").scaled(120, 120, QtCore.Qt.KeepAspectRatio,
-        #                                                                        QtCore.Qt.SmoothTransformation))
+
+        self.logo_cnpq.setPixmap(QPixmap("resources/logos/cnpq.png").scaled(190, 190, QtCore.Qt.KeepAspectRatio,
+                                                                              QtCore.Qt.SmoothTransformation))
+        self.logo_fapemig.setPixmap(QPixmap("resources/logos/fapemig.png").scaled(160, 160, QtCore.Qt.KeepAspectRatio,
+                                                                            QtCore.Qt.SmoothTransformation))
+        self.logo_if.setPixmap(QPixmap("resources/logos/if_sudeste_mg.png").scaled(220, 220, QtCore.Qt.KeepAspectRatio,
+                                                                            QtCore.Qt.SmoothTransformation))
 
         about_layout = QVBoxLayout()
         about_layout.addWidget(about_title)
+        about_layout.addWidget(about_version)
         about_layout.addWidget(about_text)
         about_layout.addWidget(about_authors)
-        about_layout.addWidget(about_text2)
         about_layout.addWidget(about_page)
         about_layout.addWidget(about_github)
-        about_layout.addWidget(about_version)
+        about_layout.addWidget(about_text2)
 
+        logo_layout = QHBoxLayout()
+        logo_layout.addWidget(self.logo_if)
+        logo_layout.addWidget(self.logo_fapemig)
+        logo_layout.addWidget(self.logo_cnpq)
+        logo_layout.setAlignment(QtCore.Qt.AlignCenter)
+
+        about_layout.addLayout(logo_layout)
         self.setLayout(about_layout)
