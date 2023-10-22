480e045620d980261007bf1750261b4c2b6559c8
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2023-10-22 18:24:41 -0300

    Update wizard helper texts (#419)

--- source/store/help_button_text.py
+++ source/store/help_button_text.py
@@ -2,23 +2,31 @@ equation = "<p> On this screen, you can insert a condition like as mathematical 
                "the numeric invariants and operations. Each one must be applied to a graph, as a function “f(G)” and" \
                " compositions. Conditions can be combined using logical connectors AND or OR. <br> Examples:" \
                " <br>\u03c9(G) == a(\u2113(G)) <br> \u03bb \u2081(G) > (\u03bb\u2081(c(G)))/2 <br>" \
-           "\u03c9(G) == a(\u2113(G)) OR \u03bb \u2081(G) > (\u03bb\u2081(c(G)))/2 </p>"
+               "\u03c9(G) == a(\u2113(G)) OR \u03bb \u2081(G) > (\u03bb\u2081(c(G)))/2 <br> <br> You can access our " \
+               "<a href=""https://github.com/GraphFilter/GraphFilter/wiki/Dictionary""> dictionary</a> " \
+               "to find out more about each invariant.</p>"
 
 method = "<p> The 'Filter graphs'' will filter a list of graphs and return all those that satisfy the conditions " \
-             "marked in the previous screens. <br> The 'Find Counterexample' will run until it finds a graph that " \
-             "does not meet the conditions, if any. </p>"
+             "which will be marked on the next screens. <br> The 'Find an example' will run until it finds a first " \
+             "graph that does meet the conditions, if any. <br> The 'Blank project' will allow you to draw your " \
+             "own graphs. "
 
 conditions = "<p> In this screen you will enter the conditions of the true/false type. If you want filtering to " \
                  "satisfy condition X, then mark it as true. If you want X not to be satisfied, check false. If it is" \
                  " an irrelevant condition, just don't check it. <br>" \
-                 "Feel free to check as many conditions as you like. </p>"
+                 "Feel free to check as many conditions as you like. <br> <br> You can access our " \
+                 "<a href=""https://github.com/GraphFilter/GraphFilter/wiki/Dictionary""> dictionary</a> " \
+                 "to find out more about each invariant.</p>"
 
 files = "<p> In this screen you must insert the files containing the lists of the graphs to be analyzed. The " \
             "files must be in the .g6 or .txt format and can contain several graphs. \n" \
-            "Accepted file formats: .g6, .txt, .g6.gz and .txt.gz</p> \n"\
+            "Accepted file formats: .g6, .txt, .g6.gz and .txt.gz.</p> \n"\
+            "If you don't have any files in these formats on your computer, we've provided two buttons in the bottom " \
+            "right-hand corner of the screen with reliable sources that have a variety of downloadable graph lists " \
+            "that can be used in the software. <br> <br>" \
             "It is strongly recommended that you avoid compressed files (.gz) as it considerably increases the" \
-        " filtering time. Use a file decompressor softweare (7zip, winzip or winrar) and load here with a " \
-        ".txt or .g6 file."
+            " filtering time. Use a file decompressor software (7zip, winzip or winrar) and load here with a " \
+            ".txt or .g6 file."
 
 review = "<p> This is the last screen before starting to filter. Here you can review all the parameters " \
              "inserted. </p>"
