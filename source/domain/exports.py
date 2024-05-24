bfcf8b5699682b31c5b893ba7fc353f6e044e0b6
Fernando Pimenta <108306295+fsoupimenta@users.noreply.github.com>
2024-05-24 15:52:47 -0300

    Improving export in a single pdf

--- source/domain/exports.py
+++ source/domain/exports.py
@@ -1,11 +1,13 @@
 import math
 import os
+from typing import Callable
 
 import matplotlib.pyplot as plt
 
 import networkx as nx
 import xlsxwriter
 import network2tikz as nxtikz
+from networkx import NetworkXError
 
 from source.domain.utils import convert_g6_to_nx
 from source.store.operations_invariants import dic_invariants_to_visualize as dic
@@ -36,40 +38,55 @@ def export_g6_to_pdf(g6code, folder, count):
     plt.close()
 
 
-def export_g6_list_to_pdf(g6codes, file_path, number_rows, number_cols, loading_window, update_loading):
-    num_graphs = len(g6codes)
-    graphs_per_page = number_rows * number_cols
-    num_pages = math.ceil(num_graphs / graphs_per_page)
+def draw_graph(graph, ax, idx, node_size, font_size):
+    nx.draw_networkx(graph, ax=ax, node_color='white', edgecolors='black',
+                     labels={node: node for node in nx.nodes(graph)}, node_size=node_size,
+                     font_size=font_size)
+    ax.set_title(f"Graph {idx + 1}", fontsize=font_size)
+
+
+def plot_graphs_on_page(g6_graphs, start_index, end_index, number_rows, number_cols, node_size, font_size,
+                        last_page=False):
+    fig, axs = plt.subplots(number_rows, number_cols, figsize=(8.27, 11.69), tight_layout=True)
+    for i, idx in enumerate(range(start_index, end_index)):
+        row, col = divmod(i, number_cols)
+        ax = axs[row, col] if number_rows > 1 else axs[col] if number_cols > 1 else axs
+        try:
+            graph = nx.from_graph6_bytes(g6_graphs[idx].encode('utf-8'))
+            draw_graph(graph, ax, idx, node_size, font_size)
+        except NetworkXError:
+            i -= 1
+
+    if last_page:
+        for j in range(i + 1, number_rows * number_cols):
+            fig.delaxes(axs.flatten()[j])
+
+    return fig
+
+
+def export_g6_list_to_pdf(g6_graphs: list[str], file_path: str, number_rows: int, number_cols: int,
+                          loading_window: object, update_loading: Callable):
+    number_graphs = len(g6_graphs)
+    elements_per_page = number_rows * number_cols
+    number_pages = math.ceil(number_graphs / elements_per_page)
+    max_dimension = max(number_rows, number_cols)
+    node_size = 500 / max_dimension
+    font_size = 18 / (max_dimension ** (1 / 1.618))
 
     with PdfPages(file_path) as pdf:
-        for page in range(num_pages):
-            start_idx = page * graphs_per_page
-            end_idx = min((page + 1) * graphs_per_page, num_graphs)
-            remaining_graphs = end_idx - start_idx
-            rows = min(remaining_graphs, number_rows)
-
-            fig, axs = plt.subplots(rows, number_cols, figsize=(8.27, 11.69), tight_layout=True)
-
-            for i, idx in enumerate(range(start_idx, end_idx)):
-                row = i // number_cols
-                col = i % number_cols
-
-                graph = nx.from_graph6_bytes(g6codes[idx].encode('utf-8'))
-                ax = axs[row, col] if rows > 1 else axs[col]
-                nx.draw_networkx(graph, ax=ax, node_color='#EEF25C', labels={item: item for item in nx.nodes(graph)})
-                ax.set_title(f"Graph {idx + 1}")
-
-                if loading_window.is_forced_to_close:
-                    loading_window.is_forced_to_close = False
-                    return
-
-                update_loading(idx)
-
-            for j in range(i + 1, number_rows * number_cols):
-                fig.delaxes(axs.flatten()[j])
-
+        for page in range(number_pages):
+            start_index = page * elements_per_page
+            end_index = min((page + 1) * elements_per_page, number_graphs)
+            last_page = page == number_pages - 1
+            fig = plot_graphs_on_page(g6_graphs, start_index, end_index, number_rows, number_cols, node_size, font_size,
+                                      last_page)
             pdf.savefig(fig)
-            plt.close()
+            plt.close(fig)
+
+            if loading_window.is_forced_to_close:
+                loading_window.is_forced_to_close = False
+                return
+            update_loading(end_index - 1)
 
 
 def export_g6_to_sheet(graph_list, invariants, file_name, update_progress, loading_window):
